f8145408d0acab96b165009cc52e1f66bee893a8
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-10-26 19:10:11 -0500

    Added components

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navigation from "../Navigation";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>The Middle Man</h1>
+      <Navigation></Navigation>
+    </header>
+  );
+}
