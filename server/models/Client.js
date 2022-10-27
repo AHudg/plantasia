63de8cdcb01541cc7b4bbe01b5026509707d5a47
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-10-27 13:25:51 -0500

    Added models for client and typedefs set up

---
+++ server/models/Client.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const clientSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+    // match: [regex expression, 'Must match a valid email address.']
+  },
+  password: {
+    type: String,
+    require: true,
+    // minLength or some password validators to make it strong
+  },
+  //   vendorList: [
+  //     {
+  //       type: Schema.Types.ObjectId,
+  //       ref: "Vendor",
+  //     },
+  //   ],
+  //   past: [
+  //     {
+  //       type: Schema.Types.ObjectId,
+  //       ref: "Item",
+  //     },
+  //   ],
+});
+
+const Client = model("Client", clientSchema);
+
+module.exports = Client;
--- server/schemas/typeDef.js
+++ server/schemas/typeDef.js
@@ -1,11 +1,30 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-const typeDefs = gql` 
-type Query{
-    helloWorld: String
-}
+const typeDefs = gql`
+  type Client {
+    _id: ID
+    username: String!
+    email: String!
+    vendorList: [Vendor]
+  }
+
+  type Vendor {
+    _id: ID
+    username: String!
+    email: String!
+    description: String
+    phone: Int
+    clientList: [Client]
+    inventory: [Item]
+  }
+
+  type Item {
+    _id: ID
+    name: String!
+    stock: Int!
+    price: Float!
+    vendor: Vendor!
+  }
 `;
 
-
-module.exports = typeDefs; 
-
+module.exports = typeDefs;
