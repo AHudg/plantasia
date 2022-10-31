59a39146dccd9637f6ffdb582cf5d1c0a807ca7a
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-10-31 14:34:14 -0500

    Created Footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      <p>Contact us!</p>
+    <div className="row justify-content-start align-items-center mx-1">
+      <p className="col-11 text-center col-md-4 text-md-start contact">
+        Contact us!
+      </p>
+      <p className="col-11 text-center col-md-4 text-md-start contactInfo">
+        Phone: (555) 555-5555
+      </p>
+      <p className="col-11 text-center col-md-4 text-md-start contactInfo">
+        Email: customersupport@middleman.com
+      </p>
     </div>
   );
 }
