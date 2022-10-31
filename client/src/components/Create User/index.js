bd588971c4f73b40acfbd966b1f19970a1f6aee4
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-10-30 21:05:22 -0500

    basic create user set up

---
+++ client/src/components/Create User/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+
+const createUser = () => {
+
+    // TODO form handlers can go here 
+
+    return (
+        <div>
+            <form>
+            <input
+                    className='input'
+                    placeholder='username'
+                    name='username'
+                    type='username'
+                    id='username'
+                    // value, and onChange can be added here
+                >
+                </input>
+                <input
+                    className='input'
+                    placeholder='email'
+                    name='email'
+                    type='email'
+                    id='email'
+                    // value, and onChange can be added here
+                >
+                </input>
+                <input
+                    className='input'
+                    placeholder='password'
+                    name='password'
+                    type='password'
+                    id='password'
+                    // value, and onChange can be added here
+                >
+                </input>
+                <button type="submit">
+                Submit
+              </button>
+            </form>
+        </div>
+    )
+
+
+}
+
+export default createUser; 
