ed942e8cb93e03f262f9a5d1f0ae735c453aac9c
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-10-30 21:14:54 -0500

    added bootstrap, attempting to do handling for createuser

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
